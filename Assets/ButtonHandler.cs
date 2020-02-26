1d2b76bca65f4f309a7fb80a49689dbefd7f57eb
IliasElAyadi <56545094+IliasElAyadi@users.noreply.github.com>
2020-02-26 13:27:28 +0100

    InitialCommit

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using GoogleARCore.Examples.ObjectManipulation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour {
+
+	GameObject pwn;
+	public void ChangeObjectTypeCercleRed()
+	{
+		pwn = GameObject.Find("Pawn Generator");
+		pwn.GetComponent<PawnManipulator>().SetIndex(0);
+	}
+	public void ChangeObjectTsypeToCercleGreen()
+	{
+		pwn = GameObject.Find("Pawn Generator");
+		pwn.GetComponent<PawnManipulator>().SetIndex(1);
+		
+	}
+	public void ChangeObjectTypeToArrow()
+	{
+		pwn = GameObject.Find("Pawn Generator");
+		pwn.GetComponent<PawnManipulator>().SetIndex(2);
+		
+	}
+	
+}
